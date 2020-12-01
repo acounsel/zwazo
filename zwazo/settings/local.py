1c1aeb161c4fe79783a5865326eca3d85d8ab40d
Samer <sarrabi@gmail.com>
2020-12-01 13:25:21 -0800

    change settings configuration

---
+++ zwazo/settings/local.py
@@ -0,0 +1,27 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '.ngrok.io', 'localhost']
+
+INSTALLED_APPS += ['debug_toolbar']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'zwazo',
+#         'USER': 'samer',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+STATIC_URL = 'static/'
---
+++ zwazo/settings/production.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['zwazo.accountabilityconsole.com', 
+    'zwazo.herokuapp.com']
+
+SECURE_SSL_REDIRECT = True
+
+STATIC_URL = 'https://{0}/{1}/'.format(
+    AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
